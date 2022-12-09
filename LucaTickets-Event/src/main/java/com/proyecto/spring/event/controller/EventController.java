f847977491e727b438ba78d6ab08e9408ff4ac9f
Joel Pascual <joeltrabajo92@gmail.com>
2022-12-09 16:42:49 +0100

    modified:   LucaTickets-Event/src/main/java/com/proyecto/spring/event/controller/EventController.java 	modified:   LucaTickets-Event/src/main/java/com/proyecto/spring/event/model/Event.java 	modified:   LucaTickets-Event/src/main/java/com/proyecto/spring/event/service/EventService.java Update de eventos funcional en service y control, con documentación de swagger.

--- LucaTickets-Event/src/main/java/com/proyecto/spring/event/controller/EventController.java
+++ LucaTickets-Event/src/main/java/com/proyecto/spring/event/controller/EventController.java
@@ -4,8 +4,9 @@ package com.proyecto.spring.event.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -13,8 +14,10 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proyecto.spring.event.model.Event;
@@ -36,6 +39,36 @@ public class EventController {
 	@Autowired
 	private EventService eventService;
 
+	
+	@Operation(summary = "Eliminar un evento por Id", description = "Elimina un evento de la BDD, devuelve un opcional", tags= {"event"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Evento eliminado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Event.class)) }),
+			@ApiResponse(responseCode = "400", description = "Evento no encontrado ", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Event.class)) }),
+			})
+	@DeleteMapping("/{id}")
+	public Optional<Event> deleteById(@PathVariable Long id){
+		
+		Optional<Event> event = eventService.deleteById(id);
+		
+		return event;
+		
+	}
+	
+	@Operation(summary = "Modificar eventos", 
+			description = "Modifica un evento, si este se encuentra", tags= {"search"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista devuelta", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = EventResponse.class)) }),
+			@ApiResponse(responseCode = "400", description = "No válido (NO implementado) ", content = @Content),
+			})
+	@PutMapping("/update")
+	public Optional<Event> updateEvent(@RequestBody Event event){
+		return eventService.updateEvent(event);
+	}
+	
+	
 	@Operation(summary = "Mostrar todos los eventos disponibles", description = "Añade un evento a la BBDD, devuelve un objeto Event", tags= {"event"})
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Eventos mostrados", content = {
